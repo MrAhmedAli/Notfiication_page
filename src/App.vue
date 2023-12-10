88252d1a22a6858b8590cc94e44250d9c3ce2776
MrAhmedAli <mr.a_ali@hotmail.com>
2023-12-10 19:38:39 +0300

    test

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <Notifications/>
+</div>
+</template>
+
+<script>
+import Notifications from './components/Notifications.vue'
+
+export default {
+  
+  components: {
+    Notifications
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #040D21;
+  line-height: normal;
+  font-family: Poppins;
+}
+</style>
